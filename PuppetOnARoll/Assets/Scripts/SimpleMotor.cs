630324ae1a563c3863de498015c940e6e7ce45cd
AlexCastelan <alex_castelan@thecdm.ca>
2018-10-15 08:41:45 -0700

    Initial commit

---
+++ PuppetOnARoll/Assets/Scripts/SimpleMotor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMotor : MonoBehaviour {
+    public float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float HorizontalSpeed = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float VerticalSpeed = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        gameObject.transform.Translate(new Vector3(HorizontalSpeed, 0.0f, VerticalSpeed));
+	}
+}
