4bfc6d7cc170bd93af772fa1f758d9939f703c84
AlexCastelan <alex_castelan@thecdm.ca>
2018-10-20 20:34:36 -0700

    MovingCameraSimpleControls
    I created a simple animated hand to test the controls for the player.

--- PuppetOnARoll/Assets/Scripts/SimpleMotor.cs
+++ PuppetOnARoll/Assets/Scripts/SimpleMotor.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleMotor : MonoBehaviour {
-    public float speed = 1.0f;
+    public float speed = 4.0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,8 +12,10 @@ public class SimpleMotor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float HorizontalSpeed = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float VerticalSpeed = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        float HorizontalSpeed = HorizontalInput * speed * Time.deltaTime;
+        float VerticalInput = Input.GetAxis("Vertical");
+        float VerticalSpeed = VerticalInput * speed * Time.deltaTime;
 
         gameObject.transform.Translate(new Vector3(HorizontalSpeed, 0.0f, VerticalSpeed));
 	}
