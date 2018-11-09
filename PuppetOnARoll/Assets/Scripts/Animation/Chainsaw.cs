785b71cc3ecd861d7d37e32ffb90e0b91fb3cd0a
Dragonliger <jalejandrocastelani@gmail.com>
2018-11-09 03:04:11 -0800

    Tortuos Night

---
+++ PuppetOnARoll/Assets/Scripts/Animation/Chainsaw.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chainsaw : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void ToolGrabbed()
+    {
+        gameObject.GetComponent<Animator>().SetBool(0, true);
+    }
+
+    public void ToolDropped()
+    {
+        gameObject.GetComponent<Animator>().SetBool(0, false);
+    }
+}
